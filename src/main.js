6d41a7dd930c9a16d4aa5c8f6714890a0490cd6a
Abdulmassih <abdalmassehsaba@gmail.com>
2023-09-04 14:01:17 +0200

    for of loop sample

--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
 class Main {
+
+    #peoples = [
+        'Robert Smith',
+        'Billy Idol',
+        'Nina Hagen',
+        'Sid Vicious'
+    ];
     constructor() {
         this.#run()
     }
@@ -6,6 +13,24 @@ class Main {
     #run() {
         const title = document.querySelector('h1')
         title.innerText = 'CD Application'
+
+        // Add an element in the DOM
+        const titleLevel2 = document.createElement('h2');
+        titleLevel2.innerText = 'Titre niveau 2';
+
+        // Hook the new DOM Element to an existing element
+        document.body.appendChild(titleLevel2);
+        const ul = document.createElement('ul');
+        for(const people of this.#peoples) {
+            const li = document.createElement('li');
+            if(people !== 'Billy Idol') {
+                li.innerText = people;
+            } else {
+                li.innerText = `<strong>${people}</strong>`;
+            }
+            ul.appendChild(li);
+        }
+        document.body.appendChild(ul);
     }
 }
 
